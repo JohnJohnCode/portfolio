adecd717e0766d443b910a9c117f9e635a4689da
JohnJohnCode <53259911+JohnJohnCode@users.noreply.github.com>
2023-07-28 21:17:56 +0200

    Update ProjectCarousel.tsx
    change image source path to fix build issues

--- src/components/ProjectCarousel/ProjectCarousel.tsx
+++ src/components/ProjectCarousel/ProjectCarousel.tsx
@@ -4,42 +4,42 @@ import CarouselItem from "./CarouselItem/CarouselItem";
 
 const projects = [
   {
-    srcPath: "src/assets/projects/members-only.png",
+    srcPath: "./assets/projects/members-only.png",
     description: `This project was created to showcase my acquired knowledge of Node, Express, Mongo and Pug. The goal of this project was to create a simple message board where people can sign up and enter passcodes to gain special priviledges.`,
     title: "Members Only",
     link: "https://github.com/JohnJohnCode/members-only",
     alt: "Screenshot of my project",
   },
   {
-    srcPath: "src/assets/projects/battleship.png",
+    srcPath: "./assets/projects/battleship.png",
     description: `This project is my remake of the classic game Battleship where the user plays against an AI. Players take turns in guessing the location of opponent's ships with the goal of sinking all of the ships. This project is written in React.`,
     title: "Battleship",
     link: "https://github.com/JohnJohnCode/battleship",
     alt: "Screenshot of my project",
   },
   {
-    srcPath: "src/assets/projects/TNW.jpg",
+    srcPath: "./assets/projects/TNW.jpg",
     description: `In this project I attempted to clone 'The Next Web' site and it's responsiveness. This project was written in plain HTML + CSS with the goal of practicing these and also to further my skills in making stuff responsive.`,
     title: "TNW clone",
     link: "https://github.com/JohnJohnCode/TNW-clone",
     alt: "Screenshot of my project",
   },
   {
-    srcPath: "src/assets/projects/weather-app.png",
+    srcPath: "./assets/projects/weather-app.png",
     description: `In this project I have a go at making a website that displays weather information on the city of your choice. The goal of this project was to further my skills in working with APIs and React.`,
     title: "Weather App",
     link: "https://github.com/JohnJohnCode/weather-app",
     alt: "Screenshot of my project",
   },
   {
-    srcPath: "src/assets/projects/KBstore.png",
+    srcPath: "./assets/projects/KBstore.png",
     description: `Keyboard Store is a Node, Express, Mongo and Pug app. The goal of this project was to create a library / store of sorts and the theme I have decided to go with, as a true gamer, was keyboards. A user is able to CRUD a keyboard, manufacturers of keyboards and keyboard categories.`,
     title: "Keyboard Store",
     link: "https://github.com/JohnJohnCode/keyboard-store",
     alt: "Screenshot of my project",
   },
   {
-    srcPath: "src/assets/projects/portfolio.png",
+    srcPath: "./assets/projects/portfolio.png",
     description: `This project is the very site you're on! The goal was to present myself, my projects and tech stack. This portfolio was initialised with Vite and written in React.`,
     title: "Portfolio",
     link: "https://github.com/JohnJohnCode/portfolio",
